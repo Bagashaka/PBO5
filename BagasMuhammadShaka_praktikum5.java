13f46c56fa4e745d0342ce7502da24022805a9a0
Bagas Muhammad Shaka <89391428+Bagashaka@users.noreply.github.com>
2022-09-21 20:51:28 +0700

    Update BagasMuhammadShaka_praktikum5.java

--- BagasMuhammadShaka_praktikum5.java
+++ BagasMuhammadShaka_praktikum5.java
@@ -32,10 +32,48 @@ public class BagasMuhammadShaka_praktikum5 {
         int idx = fullName.indexOf("gas");
         System.out.println(idx);
         
-//        int n = input.nextInt();
-//        for(int i = 0; i < n; i++){
-//         String
+// package com.mycompany.bagasmuhammadshaka_praktikum5;
+// import java.util.Scanner;
+
+// public class BagasMuhammadShaka_praktikum5 {
+
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner (System.in);       
+//         int n = input.nextInt();
+
+//         String[] arr;
+//         arr = new String[n];
+               
+//         for(int i = 0; i < n; i++){
+//         arr[i] = input.next();
+//         }
+        
+//         String kalimat="";
+//         for(int i = 0; i < n; i++){
+//             kalimat =kalimat+arr[i]+" ";
+//         }     
+//         System.out.println(kalimat);
+//No 2
+//        String str = input.nextLine();
+//        
+//        int x = input.nextInt();
+//        int y = input.nextInt();
+//        
+//        System.out.println(str.substring(x, y));
+//No 3
+//        String str1 = input.nextLine();
+//        String str2 = input.nextLine();
+//        
+//        if(str1.contains(str2) == true){
+//          System.out.println("Ya");
 //        }
+//        else{
+//          System.out.println("Tidak");
+//        }
+//        
+    }
+}
+
     }
     
 }
